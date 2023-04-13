78e05e15ed497ddc2a81ce0b5020950604e25c7a
Isanghada <susu4788@naver.com>
2023-04-13 21:45:24 +0900

    Feat : 230413 프로그래머스

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
-import _202304._12.Solution1;
+import _202304._13.Solution1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Solution1 s = new Solution1();
-        System.out.println(Arrays.toString(s.solution(6, 4, new int[][] {{1, 1, 1, 0}, {1, 2, 2, 0}, {1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 3}, {0, 0, 0, 3}})));
+        System.out.println(s.solution(6, new int[]{1, 3, 2, 5, 4, 5, 2, 3}));
+        System.out.println(s.solution(4, new int[]{1, 3, 2, 5, 4, 5, 2, 3}));
+        System.out.println(s.solution(2, new int[]{1, 1, 1, 1, 2, 2, 2, 3}));
     }
 }
