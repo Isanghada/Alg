b395abe92264160f80f4658c38c9ea923cfdca80
Isanghada <susu4788@naver.com>
2023-04-17 21:30:29 +0900

    Feat : 230417 프로그래머스(마법의 엘리베이터)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-import _202304._16_Solution_1;
+import _202304._17_Solution_1;
 
 public class Main {
     public static void main(String[] args) {
-        _16_Solution_1 s = new _16_Solution_1();
-        System.out.println(s.solution(new int[][] {{2,2,6}, {1,5,10}, {4,2,9}, {3,8,3}}, 2, 2, 3));
+        _17_Solution_1 s = new _17_Solution_1();
+        System.out.println(s.solution(16));
+        System.out.println(s.solution(2554));
     }
 }
---
+++ src/_202304/_17_Solution_1.java
@@ -0,0 +1,30 @@
+package _202304;
+
+// 전체 경우 탐색
+public class _17_Solution_1 {
+    public static int answer;  // 정답
+    public int solution(int storey) {
+        answer = Integer.MAX_VALUE;
+        calculation(storey, 0);
+        return answer;
+    }
+
+    // 최소 횟수 계산 : 재귀 활용
+    private void calculation(int storey, int value) {
+        // 현재 층이 0이면 종료
+        if(storey == 0) {
+            answer = Math.min(answer, value);
+        }
+        else{
+            // 현재 일의 자리 계산
+            int cur = storey % 10;
+            // 일의 자리 제거
+            int next = storey / 10;
+
+            // -cur로 이동
+            calculation(next, value + cur);
+            // (10-cur)로 이동 : 단, 5층 미만은 내려가는게 빠르므로 재귀 X
+            if(cur >= 5) calculation(next+1, value + (10 - cur));
+        }
+    }
+}
