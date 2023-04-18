37a2cf69adb3104cd8e935d72425ad9073256edb
Isanghada <susu4788@naver.com>
2023-04-18 21:13:18 +0900

    Feat : 230418 프로그래머스(이모티콘 할인행사..오랜만에 긴 문제.. 이해하기가 어려웠다..)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
-import _202304._17_Solution_1;
+import _202304._18_Solution_1;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _17_Solution_1 s = new _17_Solution_1();
-        System.out.println(s.solution(16));
-        System.out.println(s.solution(2554));
+        _18_Solution_1 s = new _18_Solution_1();
+        System.out.println(Arrays.toString(s.solution(new int[][]{{40, 10000}, {25, 10000}}, new int[] {7000, 9000})));
+        System.out.println(Arrays.toString(s.solution(new int[][]{{40, 2900}, {23, 10000}, {11, 5200}, {5, 5900}, {40, 3100}, {27, 9200}, {32, 6900}}, new int[] {1300, 1500, 1600, 4900})));
     }
 }
