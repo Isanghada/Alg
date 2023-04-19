af93f74d4c519d9a0ab6a4732e7ee79afc9d3502
Isanghada <susu4788@naver.com>
2023-04-19 20:56:45 +0900

    Feat : 230419 프로그래머스(너무 어렵게 생각한 문제.. 전체를 돈다고 보면 되는 것이였음을..)

--- src/_202304/_19_Solution_1.java
+++ src/_202304/_19_Solution_1.java
@@ -1,33 +1,63 @@
 package _202304;
 
+import java.util.Arrays;
+
 // 전체 경우 탐색
 public class _19_Solution_1 {
     public long solution(int[] weights) {
         long answer = 0;
-        long[] p = new long[100001];
-        p[0] = 1;
-        for(int i = 1; i <= 100000; i++) p[i] = p[i-1] * i;
-        p[0] = 0;
-        int[] count = new int[1001];
 
-        int maxW = 0;
-        for(int w : weights) {
-            maxW = Math.max(maxW, w);
-            count[w]++;
-        }
+        // 빠른 계산을 위해 정렬
+        Arrays.sort(weights);
 
-        int[] multiple = new int[]{2, 3, 4};
-        for(int i = 100; i <= maxW; i++){
-            if(count[i] != 0){
-                answer += p[count[i] - 1];
-                for(int mul : multiple){
-                    int value = i * mul;
-                    if(value <= maxW) answer += count[i] * count[value];
-                    else break;
+        int count = 0;
+        int weightLen = weights.length;
+        for(int i = 0; i <weightLen - 1; i++){
+            if(i != 0){
+                // 직전과 같으면 이전값을 빼고 더한다.
+                if (weights[i] == weights[i-1]){
+                    count--;
+                    answer += count;
+                    continue;
                 }
             }
+            // 새로운 값일 경우 0으로 초기화
+            count = 0;
+            // i 이후의 모든 수와 비교
+            for(int j = i + 1; j < weightLen; j++){
+                if(
+                        weights[i] == weights[j] ||
+                        weights[i] * 2 == weights[j] * 3 ||
+                        weights[i] * 2 == weights[j] * 4 ||
+                        weights[i] * 3 == weights[j] * 2 ||
+                        weights[i] * 3 == weights[j] * 4 ||
+                        weights[i] * 4 == weights[j] * 2 ||
+                        weights[i] * 4 == weights[j] * 3
+                ) count++;
+            }
+            answer += count;
         }
 
         return answer;
     }
+
+    // HashMap을 이용한 풀이
+    /*public long solution(int[] weights) {
+        long answer = 0;
+        Arrays.sort(weights);
+        HashMap<Double,Integer> map = new HashMap<>();
+
+        for(Integer weight:weights){
+            double w = Double.valueOf(weight);
+            if(map.containsKey(w)){
+                answer+=map.get(w);
+            }
+            map.put(w,map.getOrDefault(w,0)+1);
+            map.put(w*4/3,map.getOrDefault(w*4/3,0)+1);
+            map.put(w*1.5,map.getOrDefault(w*1.5,0)+1);
+            map.put(w*2,map.getOrDefault(w*2,0)+1);
+        }
+
+        return answer;
+    }*/
 }
