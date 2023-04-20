c4d9452eb34d64f44a7dfc3adc7add187ca252d5
Isanghada <susu4788@naver.com>
2023-04-20 20:51:52 +0900

    Feat : 230420 프로그래머스(숫자 변환하기. for로 푼거는 왜 안된거지..)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
-import _202304._19_Solution_1;
+import _202304._20_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _19_Solution_1 s = new _19_Solution_1();
-        System.out.println(s.solution(new int[]{100,180,360,100,270}));
+        _20_Solution_1 s = new _20_Solution_1();
+        System.out.println(s.solution(10, 40, 5));
+        System.out.println(s.solution(10, 40, 30));
+        System.out.println(s.solution(2, 5, 4));
     }
 }
