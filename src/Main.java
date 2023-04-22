f5fbe2c159e03c3d9507c28e8cf3c7fb40e044e7
Isanghada <susu4788@naver.com>
2023-04-23 08:23:27 +0900

    Feat : 230423 프로그래머스(무인도 여행. 가장 일반적인 완전 탐색 문제. 재밌당)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import _202304._22_Solution_1;
+import _202304._23_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _22_Solution_1 s = new _22_Solution_1();
-        System.out.println(Arrays.toString(s.solution(new int[] {2, 3, 3, 5})));
-        System.out.println(Arrays.toString(s.solution(new int[] {9, 1, 5, 3, 6, 2})));
+        _23_Solution_1 s = new _23_Solution_1();
+        System.out.println(Arrays.toString(s.solution(new String[] {"X591X","X1X5X","X231X", "1XXX1"})));
+        System.out.println(Arrays.toString(s.solution(new String[] {"XXX","XXX","XXX"})));
     }
 }
