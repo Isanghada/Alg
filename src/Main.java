4c526cbc96b0fe87d7836a7448363488bc045c27
Isanghada <susu4788@naver.com>
2023-04-24 22:52:16 +0900

    Feat : 230424 프로그래머스(호텔 대실. 누적합으로 푼 문제. 누적합 재밌어)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
-import _202304._23_Solution_1;
+import _202304._24_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _23_Solution_1 s = new _23_Solution_1();
-        System.out.println(Arrays.toString(s.solution(new String[] {"X591X","X1X5X","X231X", "1XXX1"})));
-        System.out.println(Arrays.toString(s.solution(new String[] {"XXX","XXX","XXX"})));
+        _24_Solution_1 s = new _24_Solution_1();
+        System.out.println(s.solution(new String[][] {{"15:00", "17:00"}, {"16:40", "18:20"}, {"14:20", "15:20"}, {"14:10", "19:20"}, {"18:20", "21:20"}}));
+        System.out.println(s.solution(new String[][] {{"09:10", "10:10"}, {"10:20", "12:20"}}));
+        System.out.println(s.solution(new String[][] {{"10:20", "12:30"}, {"10:20", "12:30"}, {"10:20", "12:30"}}));
     }
 }
