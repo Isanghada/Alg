53d00fab6b835be246abef461681238febe471cc
Isanghada <susu4788@naver.com>
2023-04-26 21:57:17 +0900

    Feat : 230426 프로그래머스(혼자서 하는 틱택토. 백트래킹으로 풀고 싶었는데 쌩으로 조건들 걸어서 해서 재미없음.;)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
-import _202304._25_Solution_1;
+import _202304._26_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _25_Solution_1 s = new _25_Solution_1();
-        System.out.println(s.solution(new String[] {"SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"}));
-        System.out.println(s.solution(new String[] {"LOOXS","OOOOX","OOOOO","OOOOO","EOOOO"}));
-        System.out.println(s.solution(new String[] {"SLXOX", "EXXXO", "OOOOO", "OXXXX", "OOOOO"}));
+        _26_Solution_1 s = new _26_Solution_1();
+        System.out.println(s.solution(new String[] {"O.X", ".O.", "..X"}));
+        System.out.println(s.solution(new String[] {"OOO", "...", "XXX"}));
+        System.out.println(s.solution(new String[] {"...", ".X.", "..."}));
+        System.out.println(s.solution(new String[] {"...", "...", "..."}));
+        System.out.println(s.solution(new String[] {"XXX", "XOO", "OOO"}));
     }
 }
