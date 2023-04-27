8ef089ad219198d0d0362f193ab590dfc3a36770
Isanghada <susu4788@naver.com>
2023-04-27 21:44:25 +0900

    Feat : 230426 프로그래머스(연속된 부분 수열의 합. 투 포인터 맛있다..재밌다.. 이게 알고리즘이지)

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-import _202304._26_Solution_1;
+import _202304._27_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _26_Solution_1 s = new _26_Solution_1();
-        System.out.println(s.solution(new String[] {"O.X", ".O.", "..X"}));
-        System.out.println(s.solution(new String[] {"OOO", "...", "XXX"}));
-        System.out.println(s.solution(new String[] {"...", ".X.", "..."}));
-        System.out.println(s.solution(new String[] {"...", "...", "..."}));
-        System.out.println(s.solution(new String[] {"XXX", "XOO", "OOO"}));
+        _27_Solution_1 s = new _27_Solution_1();
+        System.out.println(Arrays.toString(s.solution(new int[] {1, 2, 3, 4, 5}, 7)));
+        System.out.println(Arrays.toString(s.solution(new int[] {1, 1, 1, 2, 3, 4, 5}, 5)));
+        System.out.println(Arrays.toString(s.solution(new int[] {2, 2, 2, 2, 2}, 6)));
     }
 }
