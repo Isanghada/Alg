0ba897815c51761bdb1dd98caa2076bfea080b41
Isanghada <susu4788@naver.com>
2023-04-29 09:20:15 +0900

    Feat : 230429 프로그래머스(광물 캐기. 백트래킹인가 아닌가 좀 헛갈렸는데 맞구나..)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-import _202304._27_Solution_1;
+import _202304._29_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _27_Solution_1 s = new _27_Solution_1();
-        System.out.println(Arrays.toString(s.solution(new int[] {1, 2, 3, 4, 5}, 7)));
-        System.out.println(Arrays.toString(s.solution(new int[] {1, 1, 1, 2, 3, 4, 5}, 5)));
-        System.out.println(Arrays.toString(s.solution(new int[] {2, 2, 2, 2, 2}, 6)));
+        _29_Solution_1 s = new _29_Solution_1();
+        System.out.println(s.solution(new int[] {1, 3, 2}, new String[] {"diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone"}));
+        System.out.println(s.solution(new int[] {0, 1, 1}, new String[] {"diamond", "diamond", "diamond", "diamond", "diamond", "iron", "iron", "iron", "iron", "iron", "diamond"}));
     }
 }
