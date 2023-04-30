fecb563abbde8562300859289e6783e79859c339
Isanghada <susu4788@naver.com>
2023-04-30 11:57:41 +0900

    Feat : 230430 프로그래머스(리코챗 로봇. 전형적인 BFS 문제 조건 달 때 조심하자)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import _202304._29_Solution_1;
+import _202304._30_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _29_Solution_1 s = new _29_Solution_1();
-        System.out.println(s.solution(new int[] {1, 3, 2}, new String[] {"diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone"}));
-        System.out.println(s.solution(new int[] {0, 1, 1}, new String[] {"diamond", "diamond", "diamond", "diamond", "diamond", "iron", "iron", "iron", "iron", "iron", "diamond"}));
+        _30_Solution_1 s = new _30_Solution_1();
+        System.out.println(s.solution(new String[]{"...D..R", ".D.G...", "....D.D", "D....D.", "..D...."}));
+        System.out.println(s.solution(new String[]{".D.R", "....", ".G..", "...D"}));
     }
 }
