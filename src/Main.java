c9369d235b45ce122c416652ff1b426436874969
Isanghada <susu4788@naver.com>
2023-05-01 22:25:56 +0900

    Feat : 230501 프로그래머스(과제 진행하기. 처음에 잘못생각해서 조금 오래 걸림. 휴~)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
-import _202304._30_Solution_1;
+import _202305._01_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _30_Solution_1 s = new _30_Solution_1();
-        System.out.println(s.solution(new String[]{"...D..R", ".D.G...", "....D.D", "D....D.", "..D...."}));
-        System.out.println(s.solution(new String[]{".D.R", "....", ".G..", "...D"}));
+        _01_Solution_1 s = new _01_Solution_1();
+        System.out.println(Arrays.toString(s.solution(new String[][] {{"korean", "11:40", "30"}, {"english", "12:10", "20"}, {"math", "12:30", "40"}})));
+        System.out.println(Arrays.toString(s.solution(new String[][] {{"science", "12:40", "50"}, {"music", "12:20", "40"}, {"history", "14:00", "30"}, {"computer", "12:30", "100"}})));
+        System.out.println(Arrays.toString(s.solution(new String[][] {{"aaa", "12:00", "20"}, {"bbb", "12:10", "30"}, {"ccc", "12:40", "10"}})));
     }
 }
