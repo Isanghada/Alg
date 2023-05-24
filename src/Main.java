cb641c503ecbdb3a77bb3a390309a98dac4a7128
Isanghada <skarb4634@gmail.com>
2023-05-24 10:20:33 +0900

    Feat : 230524 프로그래머스(달리기 경주, 두 원 사이의 정수 쌍)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
-import _202305._01_Solution_1;
+import _202305._24_Solution_2;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _01_Solution_1 s = new _01_Solution_1();
-        System.out.println(Arrays.toString(s.solution(new String[][] {{"korean", "11:40", "30"}, {"english", "12:10", "20"}, {"math", "12:30", "40"}})));
-        System.out.println(Arrays.toString(s.solution(new String[][] {{"science", "12:40", "50"}, {"music", "12:20", "40"}, {"history", "14:00", "30"}, {"computer", "12:30", "100"}})));
-        System.out.println(Arrays.toString(s.solution(new String[][] {{"aaa", "12:00", "20"}, {"bbb", "12:10", "30"}, {"ccc", "12:40", "10"}})));
+        _24_Solution_2 s = new _24_Solution_2();
+        System.out.println(s.solution(2, 3));
+        System.out.println(s.solution(2, 4));
+        System.out.println(s.solution(9, 20));
+        System.out.println(s.solution(10, 20));
+        System.out.println(s.solution(999999, 1000000));
     }
 }
---
+++ src/_202305/_24_Solution_1.java
@@ -0,0 +1,36 @@
+package _202305;
+
+import java.util.*;
+
+public class _24_Solution_1 {
+    public String[] solution(String[] players, String[] callings) {
+        // 플레이어의 수
+        final int N = players.length;
+
+        // 플레이어의 현재 순위(인덱스)를 담을 Map 초기화
+        Map<String, Integer> playerMap = new HashMap<>();
+
+        // players 배열을 기준으로 Map에 데이터 입력.
+        for(int i = 0; i < N; i++){
+            playerMap.put(players[i], i);
+        }
+
+        // call에 따라 순위 변동
+        for(String call : callings){
+            // 불린 선수의 인덱스
+            int index = playerMap.get(call);
+            // 앞의 선수 위치 변경
+            // - 위치와 인덱스 모두 변경
+            players[index] = players[index-1];
+            playerMap.put(players[index], index);
+
+            // 인덱스 변경
+            index--;
+            // 불린 선수의 위치 변경
+            players[index] = call;
+            playerMap.put(call, index);
+        }
+
+        return players;
+    }
+}
