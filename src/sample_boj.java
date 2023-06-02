670361480cbb0c4ad4a4f385bcf87b9bbbb2a9a2
Isanghada <susu4788@naver.com>
2023-06-02 10:52:04 +0900

    Feat : 230602 백준(동적 프로그래밍 기초)

---
+++ src/sample_boj.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class sample_boj {
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new FileInputStream("input.txt"));
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+
+
+        System.out.println(sb);
+    }
+}
---
+++ src/sample_programmers.java
@@ -0,0 +1,6 @@
+public class sample_programmers {
+    public String solution() {
+        String answer = "Annwer";
+        return answer;
+    }
+}
