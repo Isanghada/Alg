a8277030af1a602e8da0b9665e925ee5af35fb66
Isanghada <susu4788@naver.com>
2023-07-23 19:24:05 +0900

    Feat : 230723 프로그래머스(네트워크, 유니온 파인드 연습)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-import _202305._26_Solution_1;
+import _202307._23_Solution_1;
 
 public class Main {
     public static void main(String[] args) {
-        _26_Solution_1 s = new _26_Solution_1();
-        System.out.println(s.solution(4, 5, new int[] {1, 0, 3, 1, 2}, new int[] {0, 3, 0, 4, 0}));
-        System.out.println(s.solution(2, 7, new int[] {1, 0, 2, 0, 1, 0, 2}, new int[] {0, 2, 0, 1, 0, 2, 0}));
-        System.out.println(s.solution(2, 7, new int[] {0, 0, 0, 0, 0, 0, 0}, new int[] {0, 0, 0, 0, 0, 0, 0}));
+        _23_Solution_1 s = new _23_Solution_1();
+        System.out.println(s.solution(3, new int[][] {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}));
+        System.out.println(s.solution(3, new int[][] {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}}));
     }
 }
---
+++ src/_202307/_23_Solution_1.java
@@ -0,0 +1,48 @@
+package _202307;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java
+// 유니온-파인드 연습!
+public class _23_Solution_1 {
+    public int solution(int n, int[][] computers) {
+        // 각자의 부모 정점 배열
+        int[] parent = new int[n+1];
+        // 본인 번호로 초기화
+        for(int i = 1; i <= n; i++) parent[i] = i;
+
+        // 연결된 정점에 대해 union 실행 (연결)
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i == j) continue;
+                if (computers[i][j] == 1)   union(parent, i+1, j+1);
+            }
+        }
+        
+        // 같은 정점을 제외하기 위해 Set 사용
+        Set<Integer> net = new HashSet<>();
+        // 각 네트워크의 부모 정점 추가
+        for(int i = 1; i <= n; i++){
+            int v = find(parent, i);
+            net.add(v);
+        }
+        // System.out.println(net);
+        // net의 크기가 네트워크의 개수.(서로 다른 네트워크의 수)
+        return net.size();
+    }
+
+    public static int  find(int[] parent, int x){
+        if (parent[x] != x)
+            parent[x] = find(parent, parent[x]);
+        return parent[x];
+    }
+
+    public static void union(int[] parent, int a, int b){
+        a = find(parent, a);
+        b = find(parent, b);
+
+        if(a <= b)  parent[b] = a;
+        else parent[a] = b;
+    }
+}
