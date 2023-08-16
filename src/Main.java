d9894bb783e8b8e881052ca571505c22a7aa6434
Isanghada <susu4788@naver.com>
2023-08-16 12:02:05 +0900

    Feat : 230816 프로그래머스(2차원 동전 뒤집기, 그냥 브루트 포스네.. 너무 오래 고민했어)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
-import _202307._23_Solution_1;
+import _202308._16_Solution_1;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _23_Solution_1 s = new _23_Solution_1();
-        System.out.println(s.solution(3, new int[][] {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}));
-        System.out.println(s.solution(3, new int[][] {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}}));
+        _16_Solution_1 s = new _16_Solution_1();
+//        System.out.println(Arrays.toString(s.solution(3, new int[][]{{1, 2}, {2, 3}}, new int[] {2, 3}, 1)));
+//        System.out.println(Arrays.toString(s.solution(5, new int[][] {{1, 2}, {1, 4}, {2, 4}, {2, 5}, {4, 5}}, new int[]{1, 3, 5}, 5)));
+        System.out.println(s.solution(new int[][] {{0, 1, 0, 0, 0}, {1, 0, 1, 0, 1}, {0, 1, 1, 1, 0}, {1, 0, 1, 1, 0}, {0, 1, 0, 1, 0}}, new int[][] {{0, 0, 0, 1, 1}, {0, 0, 0, 0, 1}, {0, 0, 1, 0, 1}, {0, 0, 0, 1, 0}, {0, 0, 0, 0, 1}}));
+        System.out.println(s.solution(new int[][] {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}, new int[][] {{1, 0, 1}, {0, 0, 0}, {0, 0, 0}}));
     }
 }
