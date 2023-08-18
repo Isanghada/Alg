44ab9497d3cfb5b7532f34f926a6b0b4b0b9acf3
Isanghada <susu4788@naver.com>
2023-08-18 13:15:46 +0900

    Feat : 230818 프로그래머스(숫자 타자 대회, 백트래킹인 줄 알았는데 DP 였넴 재밌당)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-import _202308._16_Solution_1;
+import _202308._18_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _16_Solution_1 s = new _16_Solution_1();
+        _18_Solution_1 s = new _18_Solution_1();
 //        System.out.println(Arrays.toString(s.solution(3, new int[][]{{1, 2}, {2, 3}}, new int[] {2, 3}, 1)));
 //        System.out.println(Arrays.toString(s.solution(5, new int[][] {{1, 2}, {1, 4}, {2, 4}, {2, 5}, {4, 5}}, new int[]{1, 3, 5}, 5)));
-        System.out.println(s.solution(new int[][] {{0, 1, 0, 0, 0}, {1, 0, 1, 0, 1}, {0, 1, 1, 1, 0}, {1, 0, 1, 1, 0}, {0, 1, 0, 1, 0}}, new int[][] {{0, 0, 0, 1, 1}, {0, 0, 0, 0, 1}, {0, 0, 1, 0, 1}, {0, 0, 0, 1, 0}, {0, 0, 0, 0, 1}}));
-        System.out.println(s.solution(new int[][] {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}, new int[][] {{1, 0, 1}, {0, 0, 0}, {0, 0, 0}}));
+        System.out.println(s.solution("1756"));
+        System.out.println(s.solution("5123"));
+        System.out.println(s.solution("7874328"));
     }
 }
