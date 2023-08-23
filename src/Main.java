967db47defb2c66094a9e112580c651876934eb7
Isanghada <susu4788@naver.com>
2023-08-24 08:17:13 +0900

    Feat : 230824 프로그래머스(억억단을 외우자, 주먹구구식 해결 후 DP 영접)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-import _202308._22_Solution_1;
+import _202308._24_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _22_Solution_1 s = new _22_Solution_1();
-//        System.out.println(Arrays.toString(s.solution(3, new int[][]{{1, 2}, {2, 3}}, new int[] {2, 3}, 1)));
-//        System.out.println(Arrays.toString(s.solution(5, new int[][] {{1, 2}, {1, 4}, {2, 4}, {2, 5}, {4, 5}}, new int[]{1, 3, 5}, 5)));
-        System.out.println(s.solution(new int[] {14, 17, 15, 18, 19, 14, 13, 16, 28, 17}, new int[][] {{10, 8}, {1, 9}, {9, 7}, {5, 4}, {1, 5}, {5, 10}, {10, 6}, {1, 3}, {10, 2}}));
-        System.out.println(s.solution(new int[] {5, 6, 5, 3, 4}, new int[][] {{2, 3}, {1, 4}, {2, 5}, {1, 2}}));
-        System.out.println(s.solution(new int[] {5, 6, 5, 1, 4}, new int[][] {{2, 3}, {1, 4}, {2, 5}, {1, 2}}));
-        System.out.println(s.solution(new int[] {10, 10, 1, 1}, new int[][] {{3, 2}, {4, 3}, {1, 4}}));
+        _24_Solution_1 s = new _24_Solution_1();
+        System.out.println(Arrays.toString(s.solution(8,new int[] {1, 3, 7})));
+//        System.out.println(s.solution(new int[] {14, 17, 15, 18, 19, 14, 13, 16, 28, 17}, new int[][] {{10, 8}, {1, 9}, {9, 7}, {5, 4}, {1, 5}, {5, 10}, {10, 6}, {1, 3}, {10, 2}}));
     }
 }
