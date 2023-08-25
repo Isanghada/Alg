ea48cfb231e45f2310752b5c69ab14bccfbbb233
Isanghada <susu4788@naver.com>
2023-08-25 12:14:40 +0900

    Feat : 230825 프로그래머스(표현 가능한 이진트리, 재귀를 통한 편안한 하루)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
-import _202308._24_Solution_1;
+import _202308._25_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _24_Solution_1 s = new _24_Solution_1();
-        System.out.println(Arrays.toString(s.solution(8,new int[] {1, 3, 7})));
+        _25_Solution_1 s = new _25_Solution_1();
+        System.out.println(Arrays.toString(s.solution(new long[] {7, 42, 5})));
+        System.out.println(Arrays.toString(s.solution(new long[] {63, 111, 95})));
 //        System.out.println(s.solution(new int[] {14, 17, 15, 18, 19, 14, 13, 16, 28, 17}, new int[][] {{10, 8}, {1, 9}, {9, 7}, {5, 4}, {1, 5}, {5, 10}, {10, 6}, {1, 3}, {10, 2}}));
     }
 }
