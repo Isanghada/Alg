8ee94ebd691c0e22980986246b31a8d27b3b8fe1
Isanghada <susu4788@naver.com>
2023-08-28 09:08:48 +0900

    Feat : 230828 프로그래머스(카운트 다운, 간단한 DP)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
-import _202308._25_Solution_1;
+import _202308._28_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _25_Solution_1 s = new _25_Solution_1();
-        System.out.println(Arrays.toString(s.solution(new long[] {7, 42, 5})));
-        System.out.println(Arrays.toString(s.solution(new long[] {63, 111, 95})));
+        _28_Solution_1 s = new _28_Solution_1();
+        System.out.println(Arrays.toString(s.solution(21)));
+        System.out.println(Arrays.toString(s.solution(58)));
 //        System.out.println(s.solution(new int[] {14, 17, 15, 18, 19, 14, 13, 16, 28, 17}, new int[][] {{10, 8}, {1, 9}, {9, 7}, {5, 4}, {1, 5}, {5, 10}, {10, 6}, {1, 3}, {10, 2}}));
     }
 }
