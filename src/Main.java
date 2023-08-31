b4efeba4a206eb30720a6d8ea613519a9983f992
Isanghada <susu4788@naver.com>
2023-08-31 11:17:21 +0900

    Feat : 230830 프로그래머스(등대, 언제나 한 발짝 더 생각하기가 어렵넹..아쉬워)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
-import _202308._30_Solution_1;
+import _202308._31_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _30_Solution_1 s = new _30_Solution_1();
+        _31_Solution_1 s = new _31_Solution_1();
 //        System.out.println(Arrays.toString(s.solution(21)));
 //        System.out.println(Arrays.toString(s.solution(58)));
-        System.out.println(s.solution(6, new int[] {1, 2, 3}));
+        System.out.println(s.solution(8, new int[][] {{1, 2}, {1, 3}, {1, 4}, {1, 5}, {5, 6}, {5, 7}, {5, 8}}));
+        System.out.println(s.solution(10, new int[][] {{4, 1}, {5, 1}, {5, 6}, {7, 6}, {1, 2}, {1, 3}, {6, 8}, {2, 9}, {9, 10}}));
     }
 }
