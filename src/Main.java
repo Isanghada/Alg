5f5216df4b9470359d46d51396a17634514bd37e
Isanghada <susu4788@naver.com>
2023-09-01 10:46:23 +0900

    Feat : 230901 프로그래머스(기지국 설치, 마음이 편안해지는 문제였구만 허허)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-import _202308._31_Solution_1;
+import _202309._01_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _31_Solution_1 s = new _31_Solution_1();
+        _01_Solution_1 s = new _01_Solution_1();
 //        System.out.println(Arrays.toString(s.solution(21)));
 //        System.out.println(Arrays.toString(s.solution(58)));
-        System.out.println(s.solution(8, new int[][] {{1, 2}, {1, 3}, {1, 4}, {1, 5}, {5, 6}, {5, 7}, {5, 8}}));
-        System.out.println(s.solution(10, new int[][] {{4, 1}, {5, 1}, {5, 6}, {7, 6}, {1, 2}, {1, 3}, {6, 8}, {2, 9}, {9, 10}}));
+        System.out.println(s.solution(11, new int[] {4, 11}, 1));
+        System.out.println(s.solution(16, new int[] {9}, 2));
+        System.out.println(s.solution(16, new int[] {1, 7}, 5));
     }
 }
