e1596c80346f13f77b46d95bd90b1dfc1dc79266
Isanghada <susu4788@naver.com>
2023-09-02 11:39:35 +0900

    Feat : 230902 프로그래머스(캠핑, 좌표 압축은 생각못했넹..오늘도 재밌다)

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-import _202309._01_Solution_1;
+import _202309._02_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _01_Solution_1 s = new _01_Solution_1();
+        _02_Solution_1 s = new _02_Solution_1();
 //        System.out.println(Arrays.toString(s.solution(21)));
 //        System.out.println(Arrays.toString(s.solution(58)));
-        System.out.println(s.solution(11, new int[] {4, 11}, 1));
-        System.out.println(s.solution(16, new int[] {9}, 2));
-        System.out.println(s.solution(16, new int[] {1, 7}, 5));
+        System.out.println(s.solution(4, new int[][] {{0, 0}, {1, 1}, {0, 2}, {2, 0}}));
     }
 }
