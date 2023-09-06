51bcfc585d83dd16a523775b2babe06c52561d57
Isanghada <susu4788@naver.com>
2023-09-06 10:34:41 +0900

    Feat : 230906 프로그래머스(보행자 천국, DP였지만 조건 생각이 까다로웠음)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
-import _202309._02_Solution_1;
+import _202309._06_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _02_Solution_1 s = new _02_Solution_1();
+        _06_Solution_1 s = new _06_Solution_1();
 //        System.out.println(Arrays.toString(s.solution(21)));
 //        System.out.println(Arrays.toString(s.solution(58)));
-        System.out.println(s.solution(4, new int[][] {{0, 0}, {1, 1}, {0, 2}, {2, 0}}));
+        System.out.println(s.solution(3, 3, new int[][] {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}));
+        System.out.println(s.solution(3, 6, new int[][] {{0, 2, 0, 0, 0, 2}, {0, 0, 2, 0, 1, 0}, {1, 0, 0, 2, 2, 0}}));
     }
 }
