bb12b62eddf60df8aecc77ea2986ed0841ff05de
Isanghada <susu4788@naver.com>
2023-09-09 15:48:19 +0900

    Feat : 230909 프로그래머스(최적의 행렬 곱셈, 익숙한 DP의 맛)

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-import _202309._08_Solution_1;
+import _202309._09_Solution_1;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _08_Solution_1 s = new _08_Solution_1();
+        _09_Solution_1 s = new _09_Solution_1();
 //        System.out.println(Arrays.toString(s.solution(21)));
 //        System.out.println(Arrays.toString(s.solution(58)));
-        System.out.println(s.solution(3, 4, 2, 3, 3, 1, 5));
-        System.out.println(s.solution(2, 2, 1, 1, 2, 2, 2));
-        System.out.println(s.solution(3, 3, 1, 2, 3, 3, 4));
+        System.out.println(s.solution(new int[][] {{5, 3}, {3, 10}, {10, 6}}));
     }
 }
