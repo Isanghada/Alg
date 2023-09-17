c1dac94e5e465080e230f9ea929b989d0237f248
Isanghada <susu4788@naver.com>
2023-09-17 12:23:01 +0900

    Feat : 230917 프로그래머스(외벽 점검, 완전 탐색)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
-import _202309._16_Solution_1;
-
-import java.util.Arrays;
+import _202309.*;
 
 public class Main {
     public static void main(String[] args) {
-        _16_Solution_1 s = new _16_Solution_1();
+        _17_Solution_1 s = new _17_Solution_1();
 //        System.out.println(Arrays.toString(s.solution()));
-        System.out.println(s.solution(2, 2, 0, 0, new int[][] {{2, 1}, {0, 1}, {1, 1}, {0, 1}, {2, 1}}));
-        System.out.println(s.solution(2, 5, 0, 1, new int[][] {{3, 1}, {2, 2}, {1, 1}, {2, 3}, {0, 1}, {2, 1}}));
+        System.out.println(s.solution(12, new int[] {1, 5, 6, 10}, new int[] {1, 2, 3, 4}));
+        System.out.println(s.solution(12, new int[] {1, 3, 4, 9, 10}, new int[] {3, 5, 7}));
     }
 }
