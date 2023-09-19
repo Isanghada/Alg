7663a4ca782dc5d7bba3cc181385cc1dea48774a
Isanghada <susu4788@naver.com>
2023-09-19 10:58:11 +0900

    Feat : 230919 프로그래머스(GPS, DP)

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import _202309.*;
 
 public class Main {
     public static void main(String[] args) {
-        _18_Solution_1 s = new _18_Solution_1();
+        _19_Solution_1 s = new _19_Solution_1();
 //        System.out.println(Arrays.toString(s.solution()));
-        System.out.println(s.solution(new int[][] {{0, 3, 3, 0}, {3, 2, 2, 3}, {0, 3, 2, 0}, {0, 3, 3, 3}}));
+        System.out.println(s.solution(7, 10, new int[][] {{1, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 4}, {3, 5}, {4, 6}, {5, 6}, {5, 7}, {6, 7}}, 6, new int[] {1, 2, 3, 3, 6, 7}));
+        System.out.println(s.solution(7, 10, new int[][] {{1, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 4}, {3, 5}, {4, 6}, {5, 6}, {5, 7}, {6, 7}}, 6, new int[] {1, 2, 3, 5, 6, 7}));
     }
 }
