3ac07af18d15d9064bf6687e681e7d74e61659af
Isanghada <susu4788@naver.com>
2023-09-20 11:56:26 +0900

    Feat : 230920 프로그래머스(기둥과 보 설치, 완전 구현 문제라 오히려 어려웠다..)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import _202309.*;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        _19_Solution_1 s = new _19_Solution_1();
+        _20_Solution_1 s = new _20_Solution_1();
+        for(int[] answer : s.solution(5, new int[][] {{1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1}}))
+            System.out.print(Arrays.toString(answer)+", ");
+        System.out.println();
+        for(int[] answer : s.solution(5, new int[][] {{0, 0, 0, 1}, {2, 0, 0, 1}, {4, 0, 0, 1}, {0, 1, 1, 1}, {1, 1, 1, 1}, {2, 1, 1, 1}, {3, 1, 1, 1}, {2, 0, 0, 0}, {1, 1, 1, 0}, {2, 2, 0, 1}}))
+            System.out.print(Arrays.toString(answer)+", ");
+        System.out.println();
 //        System.out.println(Arrays.toString(s.solution()));
-        System.out.println(s.solution(7, 10, new int[][] {{1, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 4}, {3, 5}, {4, 6}, {5, 6}, {5, 7}, {6, 7}}, 6, new int[] {1, 2, 3, 3, 6, 7}));
-        System.out.println(s.solution(7, 10, new int[][] {{1, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 4}, {3, 5}, {4, 6}, {5, 6}, {5, 7}, {6, 7}}, 6, new int[] {1, 2, 3, 5, 6, 7}));
+//        System.out.println(s.solution(7, 10, new int[][] {{1, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 4}, {3, 5}, {4, 6}, {5, 6}, {5, 7}, {6, 7}}, 6, new int[] {1, 2, 3, 3, 6, 7}));
     }
 }
