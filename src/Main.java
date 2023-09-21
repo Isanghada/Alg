3c5091290cc6fb2dab5a327b970aeb1f2d5338eb
Isanghada <susu4788@naver.com>
2023-09-21 10:07:26 +0900

    Feat : 230921 프로그래머스(지형 편집, 이분 탐색은 쉬운 듯 어려운 느낌)

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _20_Solution_1 s = new _20_Solution_1();
-        for(int[] answer : s.solution(5, new int[][] {{1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1}}))
-            System.out.print(Arrays.toString(answer)+", ");
-        System.out.println();
-        for(int[] answer : s.solution(5, new int[][] {{0, 0, 0, 1}, {2, 0, 0, 1}, {4, 0, 0, 1}, {0, 1, 1, 1}, {1, 1, 1, 1}, {2, 1, 1, 1}, {3, 1, 1, 1}, {2, 0, 0, 0}, {1, 1, 1, 0}, {2, 2, 0, 1}}))
-            System.out.print(Arrays.toString(answer)+", ");
-        System.out.println();
+        _21_Solution_1 s = new _21_Solution_1();
+//        for(int[] answer : s.solution(5, new int[][] {{1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1}}))
+//            System.out.print(Arrays.toString(answer)+", ");
+//        System.out.println();
 //        System.out.println(Arrays.toString(s.solution()));
-//        System.out.println(s.solution(7, 10, new int[][] {{1, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 4}, {3, 5}, {4, 6}, {5, 6}, {5, 7}, {6, 7}}, 6, new int[] {1, 2, 3, 3, 6, 7}));
+        System.out.println(s.solution(new int[][] {{1, 2}, {2, 3}}, 3, 2));
+        System.out.println(s.solution(new int[][] {{4, 4, 3}, {3, 2, 2}, {2, 1, 0}}, 5, 3));
     }
 }
