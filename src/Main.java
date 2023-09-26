e991428c320d8e916d1664ba9410658411752c4c
Isanghada <susu4788@naver.com>
2023-09-26 09:00:04 +0900

    Feat : 230926 프로그래머스(에어컨, DP 조건이 많아 살짝 까다로웠다!)

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        _23_Solution_1 s = new _23_Solution_1();
+        _26_Solution_1 s = new _26_Solution_1();
 //        for(int[] answer : s.solution(5, new int[][] {{1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1}}))
 //            System.out.print(Arrays.toString(answer)+", ");
 //        System.out.println();
 //        System.out.println(Arrays.toString(s.solution(new int[]{1, 1, 1, 1, 1, 1, 2, 5, 8, 2, 1, 1, 4, 8, 8, 8, 12, 6, 6}, new int[] {4, 3, 1, 5, 6})));
-//        System.out.println(s.solution(new int[] {1, 1, 2, 3}));
-//        System.out.println(s.solution(new int[] {1, 2, 4, 5}));
+        System.out.println(s.solution(28, 18, 26, 10, 8, new int[] {0, 0, 1, 1, 1, 1, 1}));
+        System.out.println(s.solution(-10, -5, 5, 5, 1, new int[] {0, 0, 0, 0, 0, 1, 0}));
+        System.out.println(s.solution(11, 8, 10, 10, 1, new int[] {0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1}));
+        System.out.println(s.solution(11, 8, 10, 10, 100, new int[] {0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1}));
     }
 }
