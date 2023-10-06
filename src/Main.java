cee3733a0b0a1e6f6104455aa9354c07e5cbb189
Isanghada <susu4788@naver.com>
2023-10-06 11:20:22 +0900

    Feat : 231006 프로그래머스([3차] 자동완성, 트리와 DFS 맛있다!)

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ import _202310.*;
 
 public class Main {
     public static void main(String[] args) {
-        _05_Solution_1 s = new _05_Solution_1();
+        _06_Solution_1 s = new _06_Solution_1();
 //        for(int[] answer : s.solution(5, new int[][] {{1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1}}))
 //            System.out.print(Arrays.toString(answer)+", ");
 //        System.out.println();
 //        System.out.println(Arrays.toString(s.solution(10, new long[]{1, 3, 4, 1, 3, 1})));
-        System.out.println(s.solution(3, 1, 3, new int[][] {{1, 2, 2}, {3, 2, 3}}, new int[] {2}));
-        System.out.println(s.solution(4, 1, 4, new int[][] {{1, 2, 1}, {3, 2, 1}, {2, 4, 1}}, new int[] {2, 3}));
+        System.out.println(s.solution(new String[] {"go", "gone", "guild"}));
+        System.out.println(s.solution(new String[] {"abc", "def", "ghi", "jklm"}));
+        System.out.println(s.solution(new String[] {"word", "war", "warrior", "world"}));
     }
 }
