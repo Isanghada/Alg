1dcb54e76fff5086cdc19841259374d4f7292c4f
Isanghada <susu4788@naver.com>
2023-10-07 11:00:10 +0900

    Feat : 231007 프로그래머스(리틀 프렌즈 사천성, 조건 구현이 조금 가다로웠다)

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ import _202310.*;
 
 public class Main {
     public static void main(String[] args) {
-        _06_Solution_1 s = new _06_Solution_1();
+        _07_Solution_1 s = new _07_Solution_1();
 //        for(int[] answer : s.solution(5, new int[][] {{1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1}}))
 //            System.out.print(Arrays.toString(answer)+", ");
 //        System.out.println();
 //        System.out.println(Arrays.toString(s.solution(10, new long[]{1, 3, 4, 1, 3, 1})));
-        System.out.println(s.solution(new String[] {"go", "gone", "guild"}));
-        System.out.println(s.solution(new String[] {"abc", "def", "ghi", "jklm"}));
-        System.out.println(s.solution(new String[] {"word", "war", "warrior", "world"}));
+        System.out.println(s.solution(3, 3, new String[] {"DBA", "C*A", "CDB"}));
+        System.out.println(s.solution(2, 4, new String[] {"NRYN", "ARYA"}));
+        System.out.println(s.solution(4, 4, new String[] {".ZI.", "M.**", "MZU.", ".IU."}));
+        System.out.println(s.solution(2, 2, new String[] {"AB", "BA"}));
     }
 }
