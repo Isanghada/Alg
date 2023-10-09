27819e96ae4c1847578109329f1605099266a5c5
Isanghada <susu4788@naver.com>
2023-10-09 12:10:06 +0900

    Feat : 23100 백준(트리 트리오 중간값, BFS로 이런식의 풀이를..)

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@ import _202310.*;
 
 public class Main {
     public static void main(String[] args) {
-        _07_Solution_1 s = new _07_Solution_1();
+        _09_Solution_1 s = new _09_Solution_1();
 //        for(int[] answer : s.solution(5, new int[][] {{1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1}}))
 //            System.out.print(Arrays.toString(answer)+", ");
 //        System.out.println();
 //        System.out.println(Arrays.toString(s.solution(10, new long[]{1, 3, 4, 1, 3, 1})));
-        System.out.println(s.solution(3, 3, new String[] {"DBA", "C*A", "CDB"}));
-        System.out.println(s.solution(2, 4, new String[] {"NRYN", "ARYA"}));
-        System.out.println(s.solution(4, 4, new String[] {".ZI.", "M.**", "MZU.", ".IU."}));
-        System.out.println(s.solution(2, 2, new String[] {"AB", "BA"}));
+        System.out.println(s.solution(4, new int[][] {{1, 2}, {2, 3}, {3, 4}}));
+        System.out.println(s.solution(5, new int[][] {{1, 5}, {2, 5}, {3, 5}, {4, 5}}));
     }
 }
