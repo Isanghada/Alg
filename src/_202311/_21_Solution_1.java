e88d601af24c7a81bfff5ca803b713e06c94e1aa
Isanghada <susu4788@naver.com>
2023-11-23 10:42:48 +0900

    Feat : 231121 백준(소수 화폐, DP)

--- src/_202311/_21_Solution_1.java
+++ src/_202311/_21_Solution_1.java
@@ -3,12 +3,13 @@ package _202311;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
-// https://www.acmicpc.net/problem/4811
-// - DP를 활용해 해결
-// - [큰 알약의 수][작은 알약의 수]일 경우의 경우의 수 계산
+// https://www.acmicpc.net/problem/16400
+// - 소수를 구하고 DP를 통해 경우의 수 계산
 public class _21_Solution_1 {
-    public static long[][] dp;
+    public static final int MOD = 123456789;
     public static void main(String[] args) throws Exception {
         // 입출력 설정
         System.setIn(new FileInputStream("src/_202311/_21_input.txt"));
@@ -16,46 +17,46 @@ public class _21_Solution_1 {
 //        StringTokenizer st = new StringTokenizer(in.readLine());
         StringBuilder sb = new StringBuilder();
 
-        // 30개의 알약일 경우까지 모두 계산!
-        dp = new long[31][31];
-        dp[0][0] = 1;   // 0개인 경우의 수
-        dp[1][0] = 1;   // 1개인 경우의 수
-        dp[2][0] = 2;   // 2개인 경우의 수
+        //  물건의 값
+        int N = Integer.parseInt(in.readLine());
 
-        // DP 계산
-        calculateDP(30, 0);
+        // 소수 계산 : 에라토스테네스의 체
+        // - true : 소수 X
+        // - false : 소수 O
+        boolean[] prime = new boolean[N+1];
+        prime[0] = true;
+        prime[1] = true;
+        // 소수를 담을 리스트
+        List<Integer> primeList = new ArrayList<>();
+        // 0, 1은 소수가 아니므로 2부터 시작!
+        for(int i = 2; i <= N; i++){
+            // false인 경우 : 소수인 경우
+            if(!prime[i]){
+                // 리스트에 추가
+                primeList.add(i);
+                // i의 배수는 소수가 될 수 없으므로 true로 변환
+                int nonPrime = i + i;
+                while(nonPrime < prime.length){
+                    prime[nonPrime] = true;
+                    nonPrime += i;
+                }
+            }
+        }
 
-        // 테스트 케이스 반복!
-        while(true){
-            // 알약의 수 입력
-            int N = Integer.parseInt(in.readLine());
-            // 0인 경우 종료
-            if(N == 0) break;
-            // 계산된 값 반환
-            sb.append(dp[N][0]).append("\n");
+        // dp 초기화
+        int[] dp = new int[N+1];
+        // - 0원인 경우! 1가지 방법이 있으므로 1로 변환
+        dp[0] = 1;
+        // 모든 소수에 대해 경우의 수 계산
+        for(int num : primeList){
+            // num 이상의 수를 만들 수 있는 경우의 수 계산
+            for(int j = num; j <= N; j++){
+                dp[j] = (dp[j] + dp[j-num]) % MOD;
+            }
         }
 
         // 정답 출력
+        sb.append(dp[N]);
         System.out.println(sb);
-    }
-
-    // DP 계산 함수
-    // - big : 큰 알약의 수
-    // - small : 작은 알약의 수
-    private static long calculateDP(int big, int small) {
-        // 큰 알약이 없는 경우 남은 경우의 수는 1
-        if(big == 0) return 1;
-        // 0이 아닌 경우 : 이미 계산된 경우 이므로 값 반환
-        if(dp[big][small] != 0) return dp[big][small];
-
-        // 경우의 수 계산
-        long count = 0;
-        // 큰 알약을 사용할 경우의 수 계산
-        count += calculateDP(big-1, small+1);
-        // 작은 알약을 사용할 경우의 수 계산(단, 작은 알약이 있는 경우)
-        if(small != 0) count += calculateDP(big, small-1);
-
-        // 계산된 경우의 수 반환
-        return dp[big][small] = count;
     }
 }
